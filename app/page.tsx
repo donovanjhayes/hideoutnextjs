ae02baa3ba6ce7ad8d57337556bd1104dd39ba87
Donnie Hayes <donovanjhayes@pm.me>
2024-05-29 09:23:05 -0400

    update link

--- app/page.tsx
+++ app/page.tsx
@@ -115,7 +115,7 @@ export default function Home() {
 */}     <div>
         </div>
         <a
-          href="/booking"
+          href="hideoutvacation.com/booking"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
