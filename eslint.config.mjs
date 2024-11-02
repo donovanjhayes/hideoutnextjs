5a60508d1fa35e5fdcbfae03267598dbc1d6082a
Donnie Hayes <donovanhayes@gmail.com>
2024-11-02 12:45:31 -0400

    trying to update ESLint to avoid errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import pluginReact from "eslint-plugin-react";
 
 
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReactConfig,
+  pluginReact.configs.flat.recommended,
 ];
