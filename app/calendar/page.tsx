865b9659ff5bed1d4ab399736299cae6df1341b5
Donnie Hayes <donovanhayes@gmail.com>
2024-11-04 21:49:19 -0500

    Update page.tsx

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,12 +1,46 @@
+
+'use client'
 import React from 'react';
 import Image from "next/image";
 // import Carousel from '@/components/carousel'
+// import Script from 'next/script';
+// import BookingTool from '@/components/BookingTool'
+// import asyncBook from '@/components/BookingTool'
+import { useEffect } from 'react';
+// import Head from 'next/head';
 
 
-export default function Home() {
+
+const Home = () => {
+
+  useEffect(() => {
+    // Load the script for the BookingToolIframe
+    const script = document.createElement('script');
+    script.src = 'https://login.smoobu.com/js/Settings/BookingToolIframe.js';
+    script.type = 'text/javascript';
+    script.async = true;
+    document.body.appendChild(script);
+
+
+    script.onload = () => {
+      // Initialize the BookingToolIframe after the script loads
+      if (window.BookingToolIframe) {
+        window.BookingToolIframe.initialize({
+          url: 'https://login.smoobu.com/en/booking-tool/iframe/335356',
+          baseUrl: 'https://login.smoobu.com',
+          target: '#apartmentIframeAll',
+        });
+      }
+    };
+
+    // Clean up the script on component unmount
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl items-center justify-between font-mono text-sm lg:flex">
 {/*        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Hideout Vacation Rentals&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
@@ -23,43 +57,50 @@ export default function Home() {
             Rentals{" "}
             <Image
               src="/hideoutvacation.svg"
-              alt="Vercel Logo"
+              alt="Hideout Vacation Logo"
               className="dark:invert place-items-center"
               width={100}
               height={24}
               priority
             />
-          {/*</a>*/}
+            {/*Need to make a link back to home*/}
+{/*            <div>
+            <a
+              href="/"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className="mb-3 text-2xl font-semibold">
+                Home{" "}
+              </h2>
+            </a>
+            </div>*/}
+
+<div>
+        <p>
+        1054 Chalet calendar shown below </p>
+</div>
         </div>
+      </div>
+      <br />
+      <div>
+        <div className="flex flex-col items-center">
+                <iframe height = "300px"
+                  src="https://login.smoobu.com/en/cockpit/widget/show-calendar-iframe/1033732/c22875e49828e2655f7ba23da50b88125546858782c62311d23cc76f6c464810"
+                  loading="lazy"
+                ></iframe>
+        </div>
+
+
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-1 lg:text-left">
-        <div className ="">
-        <iframe
-          src="https://login.smoobu.com/en/cockpit/widget/show-calendar-iframe/1033732/c22875e49828e2655f7ba23da50b88125546858782c62311d23cc76f6c464810"
-          width={800}
-          height={500}
-          loading="lazy"
-        ></iframe>;
-        </div>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Book Now{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            {/*Learn about Next.js in an interactive course with&nbsp;quizzes!*/}
-            Check our calendar for availability and rates.
-          </p>
-        </a>
-      </div>
+
+
     </main>
+
   );
 }
+export default Home;
+
+
