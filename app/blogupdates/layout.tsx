4f0d1b314b3c3f25657241547f2371a8b4dea7af
Donnie Hayes <donovanhayes@gmail.com>
2025-02-08 14:57:58 -0500

    online markdown attempt Feb 8 2025

--- app/blogupdates/layout.tsx
+++ app/blogupdates/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
       <body className="bg-slate-900 text-white max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
