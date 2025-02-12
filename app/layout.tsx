1b3c901d4a66c643e4f0a3dd212f0da931b3cb84
Donnie Hayes <donovanhayes@gmail.com>
2025-02-12 15:08:20 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import type { Metadata } from "next";
 // import { Inter } from "next/font/google";
 import "./globals.css";
-// import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react"
 // import { SpeedInsights } from "@vercel/speed-insights/next"
 
 // const inter = Inter({ subsets: ["latin"] });
@@ -26,6 +26,7 @@ export default function RootLayout({
     <html lang="en">
     <body>{children}</body>
       {/*<body className={inter.className}>{children}</body>*/}
+      <Analytics/>
             <GoogleTagManager gtmId="G-B6CZTMZ81K"/>
     </html>
   );
