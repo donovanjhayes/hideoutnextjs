67c84a9b314cb2cb8918f6fd04cd7c568dee371b
Donnie Hayes <donovanhayes@gmail.com>
2025-02-12 19:13:36 -0500

    Speed insights and analytics data. WORKS NOW

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 // import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 // import { SpeedInsights } from "@vercel/speed-insights/next"
 
 // const inter = Inter({ subsets: ["latin"] });
@@ -27,6 +28,7 @@ export default function RootLayout({
     <body>{children}</body>
       {/*<body className={inter.className}>{children}</body>*/}
       <Analytics/>
+      <SpeedInsights/>
             <GoogleTagManager gtmId="G-B6CZTMZ81K"/>
     </html>
   );
